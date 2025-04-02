c00ce0f5b262af80ce213a4ad678d850c4d2a873
jgn98 <jonigadenielsen@gmail.com>
2025-04-02 11:45:52 +0200

    Something

--- ModulConfig/ModulConfig/Persistence/UserRepository.cs
+++ ModulConfig/ModulConfig/Persistence/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,36 @@ namespace ModulConfig.Persistence
             users = new List<User>();
         }
         private List<User> users;
+
+
         public User Create(params object[] parameters)
         {
-            throw new NotImplementedException();
+            if (parameters.Length < 2)
+                throw new ArgumentException("Not enough parameters. Expected: name, initials");
+
+            string name = parameters[0] as string;
+            string initials = parameters[1] as string;
+
+            if (name == null || initials == null)
+                throw new ArgumentException("Invalid parameter types. Expected: string, string");
+
+            User user = new User(name, initials);
+
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO [CASE] (Name, Email, System, CaseType, Criticality, Description, SystemId) VALUES(@Name, @Email, @System, @CaseType, @Criticality, @Description, " +
+                                                       "(SELECT SystemId FROM [SYSTEM] WHERE NAME = @System)) SELECT @@IDENTITY", con))
+                {
+                    cmd.Parameters.Add("@Name", SqlDbType.NVarChar, 50).Value = user.Name;
+                    cmd.Parameters.Add("@Initials", SqlDbType.NVarChar, 50).Value = user.Initials;
+                    
+                    users.Add(user);
+                }
+            }
+
+            return user;
         }
 
         public void Delete(params object[] parameters)
