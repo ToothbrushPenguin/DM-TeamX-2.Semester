1ca9c794d3a9e0b5915021bfcdf1fbe9ebe7e748
JeppeMarek <Jeppe_marek@hotmail.com>
2025-04-03 10:00:42 +0200

    The `RelayCommand` class has been relocated to the new namespace `ModulConfig.ViewModels.Commands`.

--- ModulConfig/ModulConfig/ViewModels/Commands/RelayCommand.cs
+++ ModulConfig/ModulConfig/ViewModels/Commands/RelayCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace ModulConfig
+namespace ModulConfig.ViewModels.Commands
 {
     public class RelayCommand : ICommand
     {
