416e603ad418fd7034c600d0f064fceb6ffb1aca
JeppeMarek <Jeppe_marek@hotmail.com>
2025-04-03 09:54:44 +0200

    Added relaycommand

---
+++ ModulConfig/ModulConfig/RelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ModulConfig
+{
+    public class RelayCommand : ICommand
+    {
+        private Func<object, bool> canExecute;
+
+        private Action<object> execute;
+
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute?.Invoke(parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+    }
+}
